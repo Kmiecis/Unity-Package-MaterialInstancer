822fac537f7ee8bc94e27110fedc185aadb939f2
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2023-09-28 09:31:50 +0200

    Included version change in SetInt method

--- Runtime/Properties/MaterialInteger.cs
+++ Runtime/Properties/MaterialInteger.cs
@@ -7,12 +7,20 @@ namespace Common.Materials
     {
         protected override void ApplyPropertyValue(Material material, int id, int value)
         {
+#if UNITY_2021
             material.SetInteger(id, value);
+#else
+            material.SetInt(id, value);
+#endif
         }
 
         protected override int ReadPropertyValue(Material material, int id)
         {
+#if UNITY_2021
             return material.GetInteger(id);
+#else
+            return material.GetInt(id);
+#endif
         }
     }
 }
